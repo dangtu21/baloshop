cc4a7bdfb608563419937b982e41fa193f963be6
TuongNguyen09 <kiettuong.nguyen0909@gmail.com>
2022-12-09 12:58:06 +0700

    tuong

---
+++ assets/js/SanPham1.js
@@ -0,0 +1,223 @@
+const product=[
+    {id:1,image:"./assets/img/img-sanpham1/balo-adidas-1.jpg",category:"Balo",name:"test",price:"890.000đ",weight:"400g",trademark:"Adidas",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:2,image:"./assets/img/img-sanpham1/balo-adidas-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Adidas",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:3,image:"./assets/img/img-sanpham1/balo-adidas-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"590.000đ",weight:"400g",trademark:"Adidas",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:4,image:"./assets/img/img-sanpham1/balo-adidas-4.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"550.000đ",weight:"400g",trademark:"Adidas",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:5,image:"./assets/img/img-sanpham1/balo-adidas-5.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"1.099.000đ",weight:"400g",trademark:"Adidas",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:6,image:"./assets/img/img-sanpham1/balo-CarharttWip-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:7,image:"./assets/img/img-sanpham1/balo-CarharttWip-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:8,image:"./assets/img/img-sanpham1/balo-CarharttWip-3.jpg",category:"Phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:9,image:"./assets/img/img-sanpham1/balo-CarharttWip-4.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:10,image:"./assets/img/img-sanpham1/balo-CarharttWip-5.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:11,image:"./assets/img/img-sanpham1/balo-Sandqvist-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"770.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:12,image:"./assets/img/img-sanpham1/balo-Nike-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"770.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:13,image:"./assets/img/img-sanpham1/balo-Coach-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"770.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:14,image:"./assets/img/img-sanpham1/balo-Coach-4.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"790.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:15,image:"./assets/img/img-sanpham1/balo-Fjallraven-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Fjallraven",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:16,image:"./assets/img/img-sanpham1/balo-Fjallraven-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Fjallraven",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:17,image:"./assets/img/img-sanpham1/balo-Nike-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Nike",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:18,image:"./assets/img/img-sanpham1/balo-Nike-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Nike",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:19,image:"./assets/img/img-sanpham1/balo-Nike-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Nike",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:20,image:"./assets/img/img-sanpham1/balo-Octopus-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"899.000đ",weight:"400g",trademark:"Octopus",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:21,image:"./assets/img/img-sanpham1/balo-Octopus-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"1.079.000đ",weight:"400g",trademark:"Octopus",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:22,image:"./assets/img/img-sanpham1/balo-Octopus-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"499.000đ",weight:"400g",trademark:"Octopus",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:23,image:"./assets/img/img-sanpham1/balo-Octopus-4.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"690.000đ",weight:"400g",trademark:"Octopus",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:24,image:"./assets/img/img-sanpham1/balo-Supreme-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"790.000đ",weight:"400g",trademark:"Supreme",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:25,image:"./assets/img/img-sanpham1/balo-Supreme-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"790.000đ",weight:"400g",trademark:"Supreme",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:26,image:"./assets/img/img-sanpham1/balo-Supreme-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"599.000đ",weight:"400g",trademark:"Supreme",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:27,image:"./assets/img/img-sanpham1/balo-Supreme-4.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"690.000đ",weight:"400g",trademark:"Supreme",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:28,image:"./assets/img/img-sanpham1/balo-Vans-2.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Vans",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:29,image:"./assets/img/img-sanpham1/balo-Puma-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Puma",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:30,image:"./assets/img/img-sanpham1/balo-Samsonite-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Samsonite",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:31,image:"./assets/img//img-sanpham1/balo-Vans-1.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Vans",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:32,image:"./assets/img/img-sanpham1/phukien-Carhartt-1.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:33,image:"./assets/img/img-sanpham1/phukien-Carhartt-2.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:34,image:"./assets/img/img-sanpham1/phukien-Carhartt-3.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:35,image:"./assets/img/img-sanpham1/balo-Fjallraven-3.jpg",category:"Balo",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"CarharttWip",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:36,image:"./assets/img/img-sanpham1/phukien-Coach-1.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"699.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:37,image:"./assets/img/img-sanpham1/phukien-Coach-2.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"690.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:38,image:"./assets/img/img-sanpham1/phukien-Coach-3.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"599.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:39,image:"./assets/img/img-sanpham1/phukien-Coach-4.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"790.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:40,image:"./assets/img/img-sanpham1/phukien-Coach-5.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"990.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:41,image:"./assets/img/img-sanpham1/phukien-Coach-6.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:42,image:"./assets/img/img-sanpham1/phukien-Coach-7.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"890.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:43,image:"./assets/img/img-sanpham1/phukien-Coach-8.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"1.039.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:44,image:"./assets/img/img-sanpham1/phukien-Fjallraven-1.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"990.000đ",weight:"400g",trademark:"Fjallraven",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:45,image:"./assets/img/img-sanpham1/phukien-Nike-1.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"999.000đ",weight:"400g",trademark:"Nike",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:46,image:"./assets/img/img-sanpham1/phukien-Vans-1.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"999.000đ",weight:"400g",trademark:"Vans",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:47,image:"./assets/img/img-sanpham1/phukien-Vans-2.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"840.000đ",weight:"400g",trademark:"Vans",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:48,image:"./assets/img/img-sanpham1/phukien-Vans-3.jpg",category:"phụ kiện",name:"Balo Du Lịch Đa Năng Chính Hãng GLB1120",price:"1.039.000đ",weight:"400g",trademark:"Vans",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:49,image:"./assets/img/img-sanpham1/clutch-Coach-1.jpg",category:"clutch",name:"Túi Vải Bố TVBH18",price:"990.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:50,image:"./assets/img/img-sanpham1/clutch-Coach-2.jpg",category:"clutch",name:"Túi Vải Bố TVBH18",price:"990.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    {id:51,image:"./assets/img/img-sanpham1/clutch-Coach-3.jpg",category:"clutch",name:"Túi Vải Bố TVBH18",price:"990.000đ",weight:"400g",trademark:"Coach",color:"red",size:"20x29x5cm",material:"Leather"},
+    ];
+
+let html=`  <span class="link-address">TRANG CHỦ / <b>CỬA HÀNG</b></span><br>
+
+            <div class="title" onclick="renderProduct(product);renderListPage(product);changePage(product)">Danh mục</div>
+            <ul class="category">
+                <li><a onclick="renderProduct(balo);renderListPage(balo);changePage(balo)" href="#">BALO</a></li>
+                <li><a onclick="renderProduct(clutch);renderListPage(clutch);changePage(clutch)" href="#">Clutch</a></li>
+                <li><a onclick="renderProduct(phukien);renderListPage(phukien);changePage(phukien)" href="#">PHỤ KIỆN</a></li>
+                <li><a onclick="renderProduct(saleOff);renderListPage(saleOff);changePage(saleOff)" href="#">SALE OFF 50%</a></li>
+            </ul>
+
+            <div class="title">Thương hiệu</div>
+            <ul class="category">
+                <li><a onclick="renderProduct(Adidas);renderListPage(Adidas);changePage(Adidas)" href="#">Adidas</a></li>
+                <li><a onclick="renderProduct(Octopus);renderListPage(Octopus);changePage(Octopus)" href="#">Octopus</a></li>
+                <li><a onclick="renderProduct(Nike);renderListPage(Nike);changePage(Nike)" href="#">Nike</a></li>
+                <li><a onclick="renderProduct(Puma);renderListPage(Puma);changePage(Puma)" href="#">Puma</a></li>
+                <li><a onclick="renderProduct(Supreme);renderListPage(Supreme);changePage(Supreme)" href="#">Supreme</a></li>
+                <li><a onclick="renderProduct(Vans);renderListPage(Vans);changePage(Vans)" href="#">Vans</a></li>
+                <li><a onclick="renderProduct(Samsonite);renderListPage(Samsonite);changePage(Samsonite)" href="#">Samsonite</a></li>
+                <li><a onclick="renderProduct(Coach);renderListPage(Coach);changePage(Coach)" href="#">Coach</a></li>
+                <li><a onclick="renderProduct(CarharttWip);renderListPage(CarharttWip);changePage(CarharttWip)" href="#">CarharttWip</a></li>
+            </ul>`
+document.getElementById("menu-aside").innerHTML=html;
+
+let perPage=12;
+let currentPage=1;
+let start=0;
+let end=perPage;
+const btnNext=document.querySelector(".btn-next");
+const btnPrev=document.querySelector(".btn-prev");
+
+function getCurrentPage(currentPage){
+    start=(currentPage-1)*perPage;
+    end=currentPage*perPage;
+}
+
+function renderProduct(array){
+    let html="";
+    let totalPage=Math.ceil(array.length/perPage);
+    const content=array.map((item,index)=>{
+        if(index>=start && index<end){
+            html+=`<li  class="product-item">
+                    <div class="product-top">
+                        <a  href="/chitiet/chitiet${item.id}.html" class="product-thumb">
+                            <img src="`+item.image+`" alt="">
+                        </a>
+                    </div>
+                    <div class="product-info">
+                        <div class="product-cat">${item.category}</div>
+                        <div class="product-name">${item.name}</div>
+                        <div class="product-price">${item.price}</div>
+                    </div>
+                </li>`;
+        }
+    })
+    document.getElementById("listProduct").innerHTML=html;
+}
+
+
+function renderListPage(array){
+    let html="";
+    let totalPage=Math.ceil(array.length/perPage);
+    html+=`<li class="active"><a href="#">${1}</a></li>`;
+    for(let i=2;i<=totalPage;i++){
+        html+=`<li><a href="#">${i}</a></li>`;
+    }
+    document.getElementById("number-page").innerHTML=html;
+}
+
+function changePage(array){
+    let totalPage=Math.ceil(array.length/perPage);
+    const currentPages=document.querySelectorAll('.number-page li');
+    console.log(currentPages);
+    for(let i=0;i<currentPages.length;i++){
+        currentPages[i].addEventListener('click',()=>{
+            const value=i+1;
+            console.log(value);
+            currentPage=value;  
+            $('.number-page li').removeClass('active');
+            currentPages[i].classList.add('active');
+            if(currentPage!=1 && currentPage<totalPage){
+                $('.btn-prev').removeClass('btn-disabled');
+                $('.btn-next').removeClass('btn-disabled');
+            }
+            if(currentPage===1){
+                $('.btn-prev').addClass('btn-disabled');
+            }
+            if(currentPage===totalPage){
+                $('.btn-next').addClass('btn-disabled');
+            }
+            getCurrentPage(currentPage);
+            renderProduct(array);
+        })
+    }
+}
+
+btnNext.addEventListener('click',(array)=>{
+    currentPage++;
+    let totalPage=Math.ceil(array.length/perPage);
+    if(currentPage>totalPage){
+        currentPage=totalPage;
+    }
+    if(currentPage===totalPage-1){
+        $('.btn-next').addClass('btn-disabled');
+    }
+    $('.btn-prev').removeClass('btn-disabled');
+    $('.number-page li').removeClass('active');
+    $(`.number-page li:eq(${currentPage-1})`).addClass('active');
+    getCurrentPage(currentPage);
+    renderProduct(array);
+})
+
+btnPrev.addEventListener('click',(array)=>{
+    currentPage--;
+    if(currentPage<=1){
+        currentPage=1;
+    }
+    if(currentPage===1){
+        $('.btn-prev').addClass('btn-disabled');
+    }
+    $('.btn-next').removeClass('btn-disabled');
+    $('.number-page li').removeClass('active');
+    $(`.number-page li:eq(${currentPage-1})`).addClass('active');
+    getCurrentPage(currentPage);
+    renderProduct(array);
+})
+
+function searchProduct(){
+    let valueSearchInput=document.getElementById("search").value;
+    let productSearch;
+    if(valueSearchInput.includes("000")){
+         productSearch= product.filter(value=>{
+            return value.price.toUpperCase().includes(valueSearchInput.toUpperCase())
+        })
+    }
+    if(valueSearchInput.toUpperCase().includes("ba".toUpperCase())||valueSearchInput.toUpperCase().includes("ph".toUpperCase())||valueSearchInput.toUpperCase().includes("clu".toUpperCase())){
+        productSearch= product.filter(value=>{
+           return value.category.toUpperCase().includes(valueSearchInput.toUpperCase())
+       })
+   }
+   /*else {
+        productSearch= product.filter(value=>{
+           return value.name.toUpperCase().includes(valueSearchInput.toUpperCase())
+       })
+   }*/
+    renderProduct(productSearch);
+    renderListPage(productSearch);
+}
+
+let balo=product.filter(value=>value.category==="Balo");
+let clutch=product.filter(value=>value.category==="clutch");
+let phukien=product.filter(value=>value.category==="phụ kiện");
+let saleOff=[product[5],product[10],product[15],product[35]];
+let Adidas=product.filter(value=>value.trademark==="Adidas");
+let Octopus=product.filter(value=>value.trademark==="Octopus");
+let Nike=product.filter(value=>value.trademark==="Nike");
+let Puma=product.filter(value=>value.trademark==="Puma");
+let Supreme=product.filter(value=>value.trademark==="Supreme");
+let Vans=product.filter(value=>value.trademark==="Vans");
+let Samsonite=product.filter(value=>value.trademark==="Samsonite");
+let Coach=product.filter(value=>value.trademark==="Coach");
+let CarharttWip=product.filter(value=>value.trademark==="CarharttWip");
+
+console.log("asdsad");
+
+
+
+
+ 
